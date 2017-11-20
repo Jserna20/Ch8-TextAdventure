2c49be5d96b939f15e2bea82e7f7182f22b8aada
Jserna20 <31867660+Jserna20@users.noreply.github.com>
2017-11-20 15:08:26 -0500

    All rooms created
    They have no real purpose so far
    Will be adding functions to rooms
    Teleporters work in transportation

--- Game.java
+++ Game.java
@@ -34,30 +34,81 @@ public class Game
      */
     private void createRooms()
     {
-        Room outside, theater, pub, lab, office;
+        Room snowyForest, snowyMountainPeak, freezerRoom, mainRoom, frozenLakeside;
+        Room j1, j2, j3, j4, d1, d2, d3, d4, d5, d6;
       
         // create the rooms
-        outside = new Room("outside the main entrance of the university");
-        theater = new Room("in a lecture theater");
-        pub = new Room("in the campus pub");
-        lab = new Room("in a computing lab");
-        office = new Room("in the computing admin office");
+        snowyForest = new Room("In snowy forest");
+        snowyMountainPeak = new Room("On snowy mountain peak");
+        freezerRoom = new Room("In a freezer");
+        mainRoom = new Room("The main room where I woke up in");
+        frozenLakeside = new Room("By a frozen lake");
+        j1 = new Room("jungle 1");
+        j2 = new Room("jungle 2");
+        j3 = new Room("jungle 3");
+        j4 = new Room("jungle 4");
+        d1 = new Room("desert 1");
+        d2 = new Room("desert 2");
+        d3 = new Room("desert 3");
+        d4 = new Room("desert 4");
+        d5 = new Room("desert 5");
+        d6 = new Room("desert 6");
         
         // initialise room exits
-        outside.setExit("east", theater);
-        outside.setExit("south", lab);
-        outside.setExit("west", pub);
+        snowyForest.setExit("east", snowyMountainPeak);
+        snowyForest.setExit("south", mainRoom);
+        snowyForest.setExit("north", freezerRoom);
 
-        theater.setExit("west", outside);
+        snowyMountainPeak.setExit("west", snowyForest);
+        snowyMountainPeak.setExit("north", frozenLakeside);
+        
+        freezerRoom.setExit("east", frozenLakeside);
+        freezerRoom.setExit("west", j1);
+        
+        frozenLakeside.setExit("west", freezerRoom);
+        frozenLakeside.setExit("south", snowyMountainPeak);
 
-        pub.setExit("east", outside);
+        mainRoom.setExit("east", snowyForest);
+        mainRoom.setExit("west", j4);
+        mainRoom.setExit("south", d2);
 
-        lab.setExit("north", outside);
-        lab.setExit("east", office);
+        j4.setExit("south", mainRoom);
+        j4.setExit("east", j3);
+        j4.setExit("north", j1);
+        
+        j3.setExit("east", j4);
+        j3.setExit("north", j2);
+        
+        j2.setExit("east", j1);
+        j2.setExit("south", j3);
+        
+        j1.setExit("west", j2);
+        j1.setExit("east", freezerRoom);
+        
+        d1.setExit("west", j3);
+        d1.setExit("east", d2);
+        d1.setExit("south", d4);
+        
+        d2.setExit("north", mainRoom);
+        d2.setExit("west", d1);
+        d2.setExit("east", d3);
+        d2.setExit("south", d5);
+        
+        d3.setExit("west", d2);
+        d3.setExit("east", snowyMountainPeak);
+        d3.setExit("south", d6);
+        
+        d4.setExit("north", d1);
+        d4.setExit("east", d5);
+        
+        d5.setExit("west", d4);
+        d5.setExit("north", d2);
+        d5.setExit("east", d6);
+        
+        d6.setExit("west", d5);
+        d6.setExit("north", d3);
 
-        office.setExit("west", lab);
-
-        currentRoom = outside;  // start game outside
+        currentRoom = mainRoom;  // start game snowyForest
     }
 
     /**
@@ -84,8 +135,10 @@ public class Game
     private void printWelcome()
     {
         System.out.println();
-        System.out.println("Welcome to the World of Zuul!");
-        System.out.println("World of Zuul is a new, incredibly boring adventure game.");
+        System.out.println("You are trapped in a place known as the Nightmare Labyrinth!");
+        System.out.println("You must defeat 6 enemies in order to escape.");
+        System.out.println("You can find treasure chests that may help you.");
+        System.out.println("Use the teleporters in order to go from room to room.");
         System.out.println("Type '" + CommandWord.HELP + "' if you need help.");
         System.out.println();
         System.out.println(currentRoom.getLongDescription());
@@ -131,7 +184,7 @@ public class Game
      */
     private void printHelp() 
     {
-        System.out.println("You are lost. You are alone. You wander");
+        System.out.println("You still need to defeat X number of enemies");
         System.out.println("around at the university.");
         System.out.println();
         System.out.println("Your command words are:");
@@ -156,7 +209,7 @@ public class Game
         Room nextRoom = currentRoom.getExit(direction);
 
         if (nextRoom == null) {
-            System.out.println("There is no door!");
+            System.out.println("There is no teleporter!");
         }
         else {
             currentRoom = nextRoom;
@@ -176,6 +229,8 @@ public class Game
             return false;
         }
         else {
+            System.out.println("Are you sure you want to quit? \nYou will have to start over.");
+            
             return true;  // signal that we want to quit
         }
     }
